946e21889f2970da229b7ca6c3457359e44d1f9b
Jerry <jerryshum2636@gmail.com>
2024-11-07 18:54:39 -0500

    Made a dropdown component that allows user to select which employee that they want to view the tasks for

---
+++ src/features/employee/EmployeeDropdown.jsx
@@ -0,0 +1,35 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useEmployees } from "../users/useEmployees";
+
+function EmployeeDropdown({ setSelectedEmployee }) {
+   const queryClient = useQueryClient();
+   const { users, isLoading, error } = useEmployees();
+
+   function handleChange(event) {
+      const selectedEmployee = users.find(
+         (employee) => employee.id === Number(event.target.value)
+      );
+      setSelectedEmployee(selectedEmployee);
+      queryClient.invalidateQueries(["tasks", selectedEmployee.id]);
+   }
+
+   if (isLoading) return <p>Loading...</p>;
+   if (error) return <p>Error loading employees</p>;
+
+   return (
+      <div>
+         <select defaultValue="" onChange={handleChange}>
+            <option value="" disabled>
+               Select an employee
+            </option>
+            {users.map((employee) => (
+               <option key={employee.id} value={employee.id}>
+                  {employee.name} | {employee.email}
+               </option>
+            ))}
+         </select>
+      </div>
+   );
+}
+
+export default EmployeeDropdown;
