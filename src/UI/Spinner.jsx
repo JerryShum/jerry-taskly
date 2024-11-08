22a5a14464e71d2fb0ab7809d51c104d63dab1e5
Jerry <jerryshum2636@gmail.com>
2024-11-07 19:09:59 -0500

    spinner styling

--- src/UI/Spinner.jsx
+++ src/UI/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
    return (
-      <div className="fixed flex justify-center items-center h-screen">
+      <div className="fixed inset-0 flex justify-center items-center">
          <div className="relative w-16 h-16">
             <div className="absolute inset-0 rounded-full bg-blue-500 opacity-75 animate-ping"></div>
             <div className="absolute inset-0 rounded-full bg-blue-500 opacity-75 animate-ping delay-200"></div>
